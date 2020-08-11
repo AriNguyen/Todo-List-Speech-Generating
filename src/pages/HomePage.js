9a611a85cc0408b7ed57bdb31b9cd52c070a621b
Dat Duong <thanhdatduong4199@gmail.com>
2020-08-11 01:29:17 -0400

    Litte changes to login form

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import Carousel from '../components/Carousel'
+import "../App.css";
 
 function HomePage(props) {
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import "../App.css";
 function LoginPage(props) {
 
     return (
         <div>
-            <p>Login</p>
+            <div>Login
+                <form>
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter Username" id="user"></input>
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter Password" id="password"></input>
+                </form>
+            </div>
         </div>
     );
 }
