dccaeb6533c2db9c115a7b2484a4fe2c419b2b68
Dat Duong <thanhdatduong4199@gmail.com>
2020-08-11 16:02:45 -0400

    Css login button

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,16 +3,20 @@ import "../App.css";
 function LoginPage(props) {
 
     return (
-        <div>
-            <form class="container">
-                <label>Username</label>
-                <input type="text" placeholder="Enter Username" id="user"></input>
-                <label>Password</label>
-                <input type="password" placeholder="Enter Password" id="password"></input>
+        <div class="form">
+            <form name="authinfo">
+                <div class="container">
+                    <div class="auth">
+                        <label>Username</label>
+                        <input type="text" placeholder="Enter Username" id="user"></input>
+                    </div>
+                    <div class="auth">
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter Password" id="password"></input>
+                        <button type="submit" id="login">Sign in</button>
+                    </div>               
+                </div>
             </form>
-            <button id="login">
-                Sign in
-            </button>
         </div>
     );
 }
