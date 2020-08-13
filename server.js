bd4ff0f96c5194df29a5865d04ecc5af1b72e0fd
Quang Nguyen <qnn23@drexel.edu>
2020-08-13 19:45:19 -0400

    server: /auth and /user received request, connected to psql and responsed successfully

--- server.js
+++ server.js
@@ -29,24 +29,24 @@ app.post("/test", function (req, res) {
 })
 
 app.post("/user", function (req, res) {
-
+  console.log(req.body);
   let username = req.body.username;
-  let plaintextPassword = req.body.plaintextPassword;
+  let password = req.body.password;
 
-  if( username == undefined || plaintextPassword == undefined ){
-    // TODO check body has username and plaintextPassword keys
+  if( username == undefined || password == undefined ){
+    // body must have username and password keys
     res.status(401);
     res.send();
-  }else if( !(typeof username === 'string') || !(typeof plaintextPassword === 'string') ){
+  }else if( !(typeof username === 'string') || !(typeof password === 'string') ){
     // check if username and password are strings
     res.status(401);
     res.send();
   }else if( username.length < 1 || username.length > 20 ){
-    // TODO check username length >= 1 and <= 20
+    // check if username length >= 1 and <= 20
     res.status(401);
     res.send();
-  }else if( plaintextPassword.length < 5 || plaintextPassword > 36 ){
-    // TODO check password length >= 5 and <= 36
+  }else if( password.length < 5 || password > 30 ){
+    // check if password length >= 5 and <= 36
     res.status(401);
     res.send();
   }
@@ -55,39 +55,40 @@ app.post("/user", function (req, res) {
   pool.query("SELECT username FROM users WHERE username = $1", [username])
   .then( function (user) {
     if( user.rows.length !== 0 ){
-      res.status(401);
-      res.send();
+      console.log("existed user")
+      res.status(401).send();
+    }else{
+      bcrypt
+        .hash(password, saltRounds)
+        .then(function (hashedPassword) {
+          pool.query(
+            "INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
+            [username, hashedPassword]
+          )
+          .then(function (response) {
+            // account successfully created
+            console.log("added user to auth_db")
+            res.status(200).send();
+          })
+          .catch(function (error) {
+            console.log(error);
+            res.status(500).send(); // server error
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+          res.status(500).send(); // server error
+        });
     }
   });
 
-  bcrypt
-    .hash(plaintextPassword, saltRounds)
-    .then(function (hashedPassword) {
-      pool.query(
-        "INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
-        [username, hashedPassword]
-      )
-      .then(function (response) {
-        // account successfully created
-        res.status(200).send();
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.status(500).send(); // server error
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(500).send(); // server error
-    });
+
 });
 
 app.post("/auth", function (req, res) {
     let username = req.body.username;
-    let plaintextPassword = req.body.plaintextPassword;
-    pool.query("SELECT hashed_password FROM users WHERE username = $1", [
-        username,
-    ])
+    let password = req.body.password;
+    pool.query("SELECT hashed_password FROM users WHERE username = $1", [username])
     .then(function (response) {
       if (response.rows.length === 0) {
         // username doesn't exist
@@ -95,7 +96,7 @@ app.post("/auth", function (req, res) {
       }
       let hashedPassword = response.rows[0].hashed_password;
       bcrypt
-      .compare(plaintextPassword, hashedPassword)
+      .compare(password, hashedPassword)
       .then(function (isSame) {
           if (isSame) {
             // password matched
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import "../App.css";
 
 class LoginPage extends React.Component {
@@ -15,30 +16,31 @@ class LoginPage extends React.Component {
   }
 
   handleChange = e => {
-    console.log(e.currentTarget.name);
     this.setState({ [e.currentTarget.name]: e.target.value})
-    console.log(this.state);
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(`${this.state.username} ${this.state.password}`);
-    fetch('/test', {
+    // fetch to /user to create new account, to /auth to verify login info
+    fetch('/auth', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         'username': this.state.username,
-        'password': this.state.password
+        'password': this.state.password,
       }),
     })
     .then( res => {
       console.log(res.status);
-      return res.json();
-    })
-    .then( function (data) {
-      console.log(data);
+      let msg;
+      if( res.status === 200 ){
+        msg = (<p>Logging in</p>);
+      }else{
+        msg = (<p>Invalid username/password</p>);
+      }
+      ReactDOM.render(msg, this.refs.message);
     })
     .catch(err => console.error(err));
   }
@@ -46,38 +48,38 @@ class LoginPage extends React.Component {
   render(){
 
     return (
-      <div class="form">
-        <form name="authinfo" class="container" onSubmit={this.handleSubmit}>
-          <div class="auth">
-
-            <label for="username">Username</label>
+      <div className="form">
+        <form name="authinfo" className="container" onSubmit={this.handleSubmit}>
+          <div className="auth">
+            <label htmlFor="username">Username</label>
             <input
               name="username"
               type="text"
-              placeholder="Enter Username"
+              placeholder="Enter Username (1-20)"
               id="user"
               value={this.state.username}
               onChange={this.handleChange}
             />
 
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input
               name="password"
               type="password"
-              placeholder="Enter Password"
+              placeholder="Enter Password (5-30)"
               id="password"
               value={this.state.password}
               onChange={this.handleChange}
             />
             <button id="login" type="submit">
-            Sign in
+              Log in
             </button>
           </div>
+          <div ref="message" className="auth"/>
         </form>
-      </div>
 
+
+    </div>
     );
-    console.log(this);
   }
 }
 
