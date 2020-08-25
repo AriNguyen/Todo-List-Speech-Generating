36408825ab53469eab09cde1cbbd518f7c644536
Quang Nguyen <qnn23@drexel.edu>
2020-08-25 14:37:48 -0400

    fix /user path handler in server

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const bodyParser = require('body-parser');
 
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
+
 const port = 4000;
-=======
-const cors = require("cors");
-const port = 3000;
->>>>>>> 81597aaddc41ca9d100380b7209038f52d9b8783
 const hostname = "localhost";
 
 const bcrypt = require("bcrypt");
@@ -31,7 +27,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
+
 app.get("/status", function (req, res) {
   // See if the X-Auth header is set
    if (!req.headers["x-auth"]) {
@@ -55,24 +51,17 @@ app.get("/status", function (req, res) {
     res.status(401).json({ error: "Invalid JWT" });
    }
 });
-=======
-app.get("/", function (req, res) {
-  console.log(req.query);
-  res.status(200);
-  res.json({"name":"tin", "product_id": "123456"});
-})
->>>>>>> 81597aaddc41ca9d100380b7209038f52d9b8783
+
 
 app.post("/user", function (req, res) {
-
   let username = req.body.username;
-  let plaintextPassword = req.body.plaintextPassword;
+  let password = req.body.password;
 
-  if( username == undefined || plaintextPassword == undefined ){
+  if( username == undefined || password == undefined ){
     // TODO check body has username and plaintextPassword keys
     res.status(401);
     res.send();
-  }else if( !(typeof username === 'string') || !(typeof plaintextPassword === 'string') ){
+  }else if( !(typeof username === 'string') || !(typeof password === 'string') ){
     // check if username and password are strings
     res.status(401);
     res.send();
@@ -80,18 +69,13 @@ app.post("/user", function (req, res) {
     // TODO check username length >= 1 and <= 20
     res.status(401);
     res.send();
-<<<<<<< HEAD
-  }else if( password.length < 5 || password > 30 ){
-    // check if password length >= 5 and <= 30
-=======
-  }else if( plaintextPassword.length < 5 || plaintextPassword > 36 ){
+  }else if( password.length < 5 || password > 36 ){
     // TODO check password length >= 5 and <= 36
->>>>>>> 81597aaddc41ca9d100380b7209038f52d9b8783
     res.status(401);
     res.send();
   }
 
-  // TODO check if username already exists
+  // check if username already exists
   pool.query("SELECT username FROM users WHERE username = $1", [username])
   .then( function (user) {
     if( user.rows.length !== 0 ){
@@ -101,7 +85,7 @@ app.post("/user", function (req, res) {
   });
 
   bcrypt
-    .hash(plaintextPassword, saltRounds)
+    .hash(password, saltRounds)
     .then(function (hashedPassword) {
       pool.query(
         "INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
@@ -134,7 +118,7 @@ app.post("/auth", function (req, res) {
       }
       let hashedPassword = response.rows[0].hashed_password;
       bcrypt
-      .compare(plaintextPassword, hashedPassword)
+      .compare(password, hashedPassword)
       .then(function (isSame) {
           if (isSame) {
             // password matched
