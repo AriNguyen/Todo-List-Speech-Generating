8ce0df280892f20508844e535fd4fd1a023a2c03
Quang Nguyen <qnn23@drexel.edu>
2020-08-28 17:16:15 -0400

    A psql table with the username as the title will be created upon a successfull sign up. Clicking the submit button on DashboardPage will insert a new item into the table.

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/user", function (req, res) {
   let password = req.body.password;
 
   if( username == undefined || password == undefined ){
-    // TODO check body has username and plaintextPassword keys
+    // TODO check body has username and password keys
     res.status(401);
     res.send();
   }else if( !(typeof username === 'string') || !(typeof password === 'string') ){
@@ -84,62 +84,79 @@ app.post("/user", function (req, res) {
     }
   });
 
+  // Create a table with 3 columns and the username as the title
+  pool.query(`CREATE TABLE ${username}(date VARCHAR(10), priority VARCHAR(1), task VARCHAR(300))`)
+  .catch(err => {console.error(err)} )
+
   bcrypt
-    .hash(password, saltRounds)
-    .then(function (hashedPassword) {
-      pool.query(
-        "INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
-        [username, hashedPassword]
-      )
-      .then(function (response) {
-        // account successfully created
-        res.status(200).send();
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.status(500).send(); // server error
-      });
+  .hash(password, saltRounds)
+  .then(function (hashedPassword) {
+    pool.query(
+      "INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
+      [username, hashedPassword]
+    )
+    .then(function (response) {
+      // account successfully created
+      res.status(200).send();
     })
     .catch(function (error) {
+      console.log(error);
+      res.status(500).send(); // server error
+    });
+  })
+  .catch(function (error) {
       console.log(error);
       res.status(500).send(); // server error
     });
 });
 
 app.post("/auth", function (req, res) {
-    let username = req.body.username;
-    let password = req.body.password;
+  let username = req.body.username;
+  let password = req.body.password;
 
-    pool.query("SELECT hashed_password FROM users WHERE username = $1", [username])
-    .then(function (response) {
-      if (response.rows.length === 0) {
-        // username doesn't exist
-        return res.status(401).send();
-      }
-      let hashedPassword = response.rows[0].hashed_password;
-      bcrypt
-      .compare(password, hashedPassword)
-      .then(function (isSame) {
-          if (isSame) {
-            // password matched
-            var token = jwt.encode({username: username}, secret);
-            res.status(302).json({'token': token});
-          } else {
-            // password didn't match
-            res.status(401).send();
-          }
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.status(500).send(); // server error
-      });
+  pool.query("SELECT hashed_password FROM users WHERE username = $1", [username])
+  .then(function (response) {
+    if (response.rows.length === 0) {
+      // username doesn't exist
+      return res.status(401).send();
+    }
+    let hashedPassword = response.rows[0].hashed_password;
+    bcrypt
+    .compare(password, hashedPassword)
+    .then(function (isSame) {
+        if (isSame) {
+          // password matched
+          var token = jwt.encode({username: username}, secret);
+          res.status(302).json({'token': token});
+        } else {
+          // password didn't match
+          res.status(401).send();
+        }
     })
     .catch(function (error) {
-        console.log(error);
-        res.status(500).send(); // server error
+      console.log(error);
+      res.status(500).send(); // server error
     });
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.status(500).send(); // server error
+  });
 });
 
+app.post("/add", function (req, res) {
+  let user = req.body.user;
+  let date = req.body.date.split("T")[0]; //cut time, in yyyy-mm-dd
+  let priority = req.body.priority;
+  let task = req.body.task;
+
+  pool.query(`INSERT INTO ${user}(date, priority, task) VALUES($1, $2, $3);`,
+                                  [date, priority, task]
+  )
+  .catch(err => {console.error(err)});
+
+  res.status(200).send();
+})
 
 
 app.listen(port, hostname, () => {
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -38,129 +38,188 @@ todoItems.push({ index: 3, value: "buy flowers", done: true });
 
 
 class DashboardPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-            display: "hide",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      date: new Date(),
+      priority: '',
+      task: '',
+      display: "hide",
     }
+  }
 
-    onChange = date => this.setState({ date })
+  componentDidMount() {
+    window.addEventListener('load', this.handleLoad);
+  }
 
-    handleClick() {
-        if (this.state.display === "hide") {
-            this.setState({
-                display: this.state.display = "show"
-            })
-        } else {
-            this.setState({
-                display: this.state.display = "hide"
-            })
-        }
+  handleLoad = e => {
+    // e.preventDefault();
+    this.setState( {'user': window.location.search.split('=')[1]})
+
+    // get user data from database and append to the DOM
+
+  }
+
+  // handle click on the calendar to set date
+  onChange = date => this.setState({ date })
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.target.value})
+  }
+
+  // handle toglge option to display/hide the speech-to-text feature
+  handleVoice() {
+    if (this.state.display === "hide") {
+      this.setState({
+        display: this.state.display = "show"
+      })
+    } else {
+      this.setState({
+        display: this.state.display = "hide"
+      })
     }
+  }
 
-    speechRecognition = function () {
-        window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-        let finalTranscript = '';
-        let recognition = new window.SpeechRecognition();
+  speechRecognition = function () {
+      window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+      let finalTranscript = '';
+      let recognition = new window.SpeechRecognition();
 
-        recognition.interimResults = true;
-        recognition.maxAlternatives = 10;
-        recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.maxAlternatives = 10;
+      recognition.continuous = true;
 
-        recognition.onresult = (event) => {
-            let interimTranscript = '';
-            for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-                let transcript = event.results[i][0].transcript;
-                if (event.results[i].isFinal) {
-                    finalTranscript += transcript;
-                } else {
-                    interimTranscript += transcript;
-                }
-            }
+      recognition.onresult = (event) => {
+          let interimTranscript = '';
+          for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+              let transcript = event.results[i][0].transcript;
+              if (event.results[i].isFinal) {
+                  finalTranscript += transcript;
+              } else {
+                  interimTranscript += transcript;
+              }
+          }
 
-            //document.querySelector('body').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
-            document.getElementById("voice content").innerHTML = finalTranscript + '' + interimTranscript;
-        }
-        recognition.start();
+          //document.querySelector('body').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+          document.getElementById("voice content").innerHTML = finalTranscript + '' + interimTranscript;
+      }
+      recognition.start();
 
-    }
+  }
 
+  // send username and new task info to /add in server
+  handleNewTask = e => {
+    e.preventDefault();
 
-    render() {
-        let myTheme = createTheme(darkTheme, lightTheme);
-        return (
-            <div className="show-fake-browser sidebar-page h-100">
-                <Container className="h-100" style={{ backgroundColor: myTheme.background, color: myTheme.text }}>
-                    <SideBar backgroundColor={myTheme.secondary} color={myTheme.text} />
-                    <Container>
-                        <Header>
-                            {/* <h2>Page Title</h2> */}
-                        </Header>
+    fetch("/add", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth': window.localStorage.token,
+      },
+      body: JSON.stringify({
+        'user': this.state.user,
+        'date': this.state.date,
+        'priority': this.state.priority,
+        'task': this.state.task,
+      })
+    })
+    .then( res => {
+      console.log(res.status);
+    })
+    .catch( err => {console.error(err) })
 
-                        <Row>
-                            <Col sm={9}>
-                                {/* Speech Recognition to add tasks */}
-                                <div>
-                                    <div id="task-container">
-                                        <div id="taskInfo">
-                                            <label for="date" className="label">Date</label>
-                                            <input type="text" name="date"></input>
-                                            <label for="priority" className="label">Priority</label>
-                                            <select name="priority">
-                                                <option value="1">1</option>
-                                                <option value="2">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                            </select>
-                                            <label for="textbox" class="label">Task</label>
-                                            <textarea class="task content" rows="4" cols="50"></textarea>
-                                        </div>
-
-                                        <div className={`voice-container ${this.state.display}`}>
-                                            <div>
-                                                <label for="textbox" className="label">Voice content</label>
-                                                <textarea id="voice content" rows="4" cols="50"></textarea>
-                                            </div>
-                                            <button class="button start" type="submit" onClick={this.speechRecognition}>Start</button>
-                                            <button class="button stop" type="submit" >Stop</button>
-                                            <button class="button load" type="submit">Load</button>
-
-                                        </div>
-
-                                        <button class="button voice" type="submit" onClick={() => this.handleClick()}>Voice Recognition</button>
-                                        <button class="button submit" type="submit">Submit</button>
+    // remove all inputs from the add task area
+    this.setState({
+      date: new Date(),
+      priority: '',
+      task: '',
+      display: 'hide',
+    })
+  }
 
 
-                                    </div>
-                                </div>
+  render() {
+    let myTheme = createTheme(darkTheme, lightTheme);
+    return (
+      <div className="show-fake-browser sidebar-page h-100">
+        <Container className="h-100" style={{ backgroundColor: myTheme.background, color: myTheme.text }}>
+          <SideBar backgroundColor={myTheme.secondary} color={myTheme.text} />
+          <Container>
+            <Header>
+              {/* <h2>Page Title</h2> */}
+            </Header>
 
-                                {/* Tasks Containers */}
-                                <Carousel />
-                            </Col>
+            <Row>
+              <Col sm={9}>
+                  {/* Speech Recognition to add tasks */}
+                  <div>
+                    <div id="task-container">
+                      <div id="taskInfo">
+                        <label for="date" className="label">Due date</label>
+                        <input
+                          type="text"
+                          name="date"
+                          value={this.state.date}
+                          onChange={this.handleChange}></input>
+                        <label for="priority" className="label">Priority (from most to least important)</label>
+                        <select name="priority" value={this.state.priority} onChange={this.handleChange}>
+                          <option value=''>---Select---</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                        </select>
+                        <label for="task" class="label">Task</label>
+                        <textarea
+                          name="task"
+                          class="task content"
+                          rows="4" cols="50"
+                          value={this.state.task}
+                          onChange={this.handleChange}></textarea>
+                      </div>
+
+                      <div className={`voice-container ${this.state.display}`}>
+                        <div>
+                          <label for="textbox" className="label">Voice content</label>
+                          <textarea id="voice content" rows="4" cols="50"></textarea>
+                        </div>
+                        <button class="button start" type="submit" onClick={this.speechRecognition}>Start</button>
+                        <button class="button stop" type="submit" >Stop</button>
+                        <button class="button load" type="submit">Load</button>
+                      </div>
+
+                    <button class="button voice" type="submit" onClick={() => this.handleVoice()}>Voice Recognition</button>
+                    <button class="button submit" type="submit" onClick={this.handleNewTask}>Submit</button>
+                    </div>
+                  </div>
+
+                  {/* Tasks Containers */}
+                  <Carousel />
+              </Col>
 
 
-                            <Col sm={3}>
-                                <DarkModeToggle lightTheme={lightTheme} darkTheme={darkTheme} />
-                                
-                                {/* Calendar */}
-                                <div id="calendar" className="p-0">
-                                    <Calendar
-                                        onChange={this.onChange}
-                                        value={this.state.date}
-                                    />
-                                </div>
+              <Col sm={3}>
+                  <DarkModeToggle lightTheme={lightTheme} darkTheme={darkTheme} />
 
-                                {/* Priority tasks */}
-                                <TodoApp initItems={todoItems} />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Container>
-            </div >
-        );
-    }
+                  {/* Calendar */}
+                  <div id="calendar" className="p-0">
+                      <Calendar
+                          value={this.state.date}
+                          onChange={this.onChange}
+                      />
+                  </div>
+
+                  {/* Priority tasks */}
+                  <TodoApp initItems={todoItems} />
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </div >
+    );
+  }
 }
 
 export default DashboardPage;
