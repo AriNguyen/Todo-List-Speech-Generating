abaccae5756cabadb6c6e69fb0da48489e0caf47
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-08-29 16:54:14 -0400

    fix css, and syntax error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -50,7 +50,7 @@ class Carousel extends React.Component {
     }
 
     render() {
-        return (sg 
+        return (
             <Container fluid={true} className="align">
                 <Row className="justify-content-around">
                     {this.makeItems(this.state.items)}
