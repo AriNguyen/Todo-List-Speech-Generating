1067a4d387adaaf5a3d3f14edc97f0804ab7b0dd
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-08-29 19:57:46 -0400

    Change: todoItems List

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -33,10 +33,7 @@ const darkTheme = {
     popcolor: "#1ffb7e"
 }
 
-var todoItems = [];
-todoItems.push({ index: 1, value: "learn react", done: false });
-todoItems.push({ index: 2, value: "Go shopping", done: true });
-todoItems.push({ index: 3, value: "buy flowers", done: true });
+var todoItems = ["learn react", "Go shopping", "buy flowers"];
 
 
 class DashboardPage extends React.Component {
