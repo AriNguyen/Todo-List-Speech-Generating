191626688cb7842ca43f35eeb14cd81251cdf13a
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-08-29 16:39:39 -0400

    add todo list with date to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import CardInfo from '../components/CardInfo'
+import TodoApp from '../components/TodoApp'
 
 function Card(props) {
-    
     return(
-        <div className="d-inline-block a-card" onClick={(e) => props.onClick(props.item)}>
-            <img className="a-card-image" src={props.item.itemSrc} alt={props.item.itemSrc}></img>
-            { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/>}
+        <div className="d-inline-block a-card overflow-auto" onClick={(e) => props.onClick(props.item)}>
+            {/* <img className="a-card-image" src={props.item.itemSrc} alt={props.item.itemSrc}></img> */}
+            {/* { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/>} */}
+            <TodoApp initItems={props.initItems} title={props.title}/>
         </div>
-    );
-
-}
+    );}
 
 export default Card;
