5a2ed24f9ab012bc53646c1bc78460b1853f5ea0
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-08-29 20:03:28 -0400

    reformat/card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,11 @@ import React from 'react';
 import TodoApp from '../components/TodoApp'
 
 function Card(props) {
-    return(
+    return (
         <div className="d-inline-block a-card overflow-auto" >
-            {/* <img className="a-card-image" src={props.item.itemSrc} alt={props.item.itemSrc}></img> */}
-            {/* { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link}/>} */}
-            <TodoApp initItems={props.initItems} title={props.title}/>
+            <TodoApp initItems={props.initItems} title={props.title} />
         </div>
-    );}
+    );
+}
 
 export default Card;
