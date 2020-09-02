1529830914c87858c51140c45aef0ca65574b7ef
Dat Duong <thanhdatduong4199@gmail.com>
2020-09-02 18:08:58 -0400

    Update theme login

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -5,7 +5,7 @@ import DarkTheme from 'react-dark-theme'
 
 function DarkModeToggle(props) {
     return (
-        <Container className="d-flex justify-content-end align-middle h-40" >
+        <Container className="d-flex justify-content-end align-middle h-40" style={{position:'absolute', top: '5px', right: '5px'}}>
             <DarkTheme light={props.lightTheme} dark={props.darkTheme} defaultDark="true" />
         </Container>
     )
