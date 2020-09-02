bf1a05689ff94db82418f1860376b95ba42e1523
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-09-02 14:34:43 -0400

    reformat DarkModeToggle

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Navbar, Nav, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import DarkTheme from 'react-dark-theme'
-// import { Container } from 'rsuite';
 
 
 function DarkModeToggle(props) {
     return (
-            <Container className="dark_theme d-flex justify-content-center pb-1 m-0">
-                <DarkTheme light={props.lightTheme} dark={props.darkTheme} defaultDark="true" />
-            </Container>
+        <Container className="d-flex justify-content-end align-middle h-40" >
+            <DarkTheme light={props.lightTheme} dark={props.darkTheme} defaultDark="true" />
+        </Container>
     )
 }
 export default DarkModeToggle;
